6408638610560dc44d82357e3a2b094392b0c40f
Blake Shucart <bs4w9@mail.missouri.edu>
2020-04-10 16:57:46 -0500

    Added Table view and set background colors on cells

---
+++ ColorsTableView/ColorsTableView/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  ColorsTableView
+//
+//  Created by Blake Shucart on 4/10/20.
+//  Copyright © 2020 Blake Shucart. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+    
+}
+
